c102f2cae8661b05524e878f2fc539f4447c25a8
apomane22 <thatomapomane44@gmail.com>
2026-04-20 09:30:29 +0200

    magwinya code

---
+++ CODE BLOCK PAT 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int temp1;
+    int temp2;
+    int temp3;
+
+    cout << "Enter the first temperature reading: ";
+    cin >> temp1;
+
+    cout << "Enter the second temperature reading: ";
+    cin >> temp2;
+
+    int increase = temp2 - temp1;
+
+    if (increase > 50) {
+        cout << "Reduce fryer heat before taking the third reading." << endl;
+    }
+    else if (increase < 10) {
+        cout << "Increase the fryer heat before taking the third reading." << endl;
+    }
+
+    cout << "Enter the third temperature reading: ";
+    cin >> temp3;
+
+    if (temp3 >= 150 && temp3 <= 190) {
+        cout << "You may start frying the Magwinyas." << endl;
+    }
+    else {
+        cout << "Oil is not ready for frying!" << endl;
+    }
+
+    return 0;
+}
